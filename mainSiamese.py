992a6c1e3a469697b3b90f5d0bbc191c24e4790f
Adrijana Ilic <adrijana.ilic@digitalstrom.com>
2021-05-13 09:20:23 +0200

    [CHG] Refactor main files

--- mainSiamese.py
+++ mainSiamese.py
@@ -2,43 +2,67 @@ from SiameseNet import SiameseNet
 from ModelHandler import ModelHandler
 from DataHandler import DataHandler
 from plotters import Plotter
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+plotter = Plotter()
 
 # %% MNIST dataset
-dh = DataHandler("MNIST", classes_to_select=[0,1,2,3,4,5,6])
-#dh_newdata = DataHandler("MNIST", classes_to_select=[7,8,9])
-
-# %% Define embedding model
-mh = ModelHandler(model_number=4, embedding_size=200, input_feature_dim=dh.shape)
+dh = DataHandler("fashion_MNIST", classes_to_select=[5, 7, 9])
+#dh = DataHandler("MNIST", classes_to_select=[7,8,9])
 
 # %% Define siamese net
-#alphas = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
-alphas = [0.1]
-for alpha in alphas:
-    net = SiameseNet(mh, dh, alpha)
-    net.print_model()
-    batch_size = 200
-    epochs = 1
-    steps_per_epoch = 1#int(dh.n_train / batch_size)
-    history = net.train("create_pair_batch_random", batch_size, epochs, steps_per_epoch)
+#alphas = [0.1, 0.5, 1, 1.5, 2]
+alphas = [1]
+epochs = [20]
+batch_sizes = [300]
+knn_ks = [5, 10, 25, 50, 75, 100]
+#knn_ks = [1]
+#embedding_sizes = [5, 10, 50, 100]
+embedding_sizes = [100]
 
-    # % Plot loss
-    # Losses
-    plotter = Plotter()
-    plotter.plot_losses(net, history)
 
-    # % Plot data MNIST
-    # TRAIN DATA ############################################################################
-    plotter.pca_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
-
-    # % TEST DATA #############################################################################
-    plotter.pca_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='Test data')
+accuracies_k = []
+for embedding_size in embedding_sizes:
+    textfile = open('pics/contrastive/embedding_size_' + str(embedding_size) + '.txt', 'w')
+    mh = ModelHandler(model_number=5, embedding_size=embedding_size, input_feature_dim=dh.shape)
+    for alpha in alphas:
+        textfile.write('alpha = ' + str(alpha) + ': ')
+        for batch_size in batch_sizes:
+            for epoch in epochs:
+                net = SiameseNet(mh, dh, alpha)
+                net.print_model()
+                
+                steps_per_epoch = 10 #int(dh.n_train / batch_size)
+                history = net.train("create_pair_batch_random", batch_size, epoch, steps_per_epoch)
     
-    # % NEW DATA #############################################################################
-    #plotter.pca_plot_compare(X=dh_newdata.X_test, y=dh_newdata.y_test, net=net, suptitle='Different data')
+                # % Plot loss
+                # Losses
+                plotter.plot_losses(net, history)
+    
+                # % Plot data MNIST
+                # TRAIN DATA ############################################################################
+                plotter.tsne_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
+                plotter.pca_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
+    
+                # % TEST DATA #############################################################################
+                plotter.tsne_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='Test data')
+                plotter.pca_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='Test data')
+        
+                #% Train kNN classifier with embedded data.
+                X_embedded_train = net.embedding_model.predict(dh.X_train)
+                X_embedded_test  = net.embedding_model.predict(dh.X_test)
+                tmp = []
+                for k in knn_ks:
+                    knn_classifier = KNeighborsClassifier(n_neighbors=k)
+                    knn_classifier.fit(X_embedded_train, dh.y_train)
+                    y_pred = knn_classifier.predict(X_embedded_test)
+                    tmp.append(accuracy_score(dh.y_test, y_pred))
+                accuracies_k.append(tmp)
+                
+                for element in tmp:
+                    textfile.write(str(element) + ", ")
+        textfile.write('\n')
+    textfile.close()
 
-# %% NEW DATA
-net.save_model()
-net1 = SiameseNet(model_name='model4_alpha0.1_epochs1_batchSize200_steps1')
-
-plotter.pca_plot_compare(X=dh.X_test, y=dh.y_test, net=net1, suptitle='hgfhfg data')
-   
+print(accuracies_k)
--- mainTriplet.py
+++ mainTriplet.py
@@ -4,111 +4,98 @@ from ModelHandler import ModelHandler
 from plotters import Plotter
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-#+ TODO tf_mean/tf_sum reduce...
 
 plotter = Plotter()
 #%% MNIST dataset
 #dh = DataHandler("MNIST")
-#plotter.tsne_plot(dh.X_train, dh.y_train, title='MNIST dataset', figname='mnist_dataset_tsne')
 
 #%% fashionMNIST
-dh = DataHandler("MNIST", [4, 9, 5])
-#plotter.tsne_plot(dh_fashion.X_train, dh_fashion.y_train, title='fashion_MNIST dataset', figname='fashion_mnist_dataset_tsne')
-
-# %% Define embedding model
-mh = ModelHandler(model_number=5, embedding_size=50, input_feature_dim=dh.shape)
+dh = DataHandler("fashion_MNIST")
 
 #%% Define triplet net
-#alphas = [1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4]
-alphas = [4]
+alphas = [0.1, 0.5, 1, 1.5, 2]
+
 #mining_methods = ['batch_all', 'batch_hard', 'create_triplet_batch_random']
 mining_methods = ['batch_hard']
-# model collapse explained:
-# https://stats.stackexchange.com/questions/475655/in-training-a-triplet-network-i-first-have-a-solid-drop-in-loss-but-eventually
-#numbers_of_samples_per_class = [5, 10, 15, 20, 25]
-numbers_of_samples_per_class = [100]
-#epochs = [5, 10, 20, 30, 50, 100, 150, 200]
-epochs = [10]
-batch_sizes = [300]
-knn_ks = [5, 10, 25, 50, 75, 100]
-#knn_ks = [1]
 
+#numbers_of_samples_per_class = [5, 10, 15, 20, 25]
+numbers_of_samples_per_class = [5]
+#epochs = [5, 10, 20, 30, 50, 100, 150, 200]
+epochs = [50]
+batch_sizes = [20]
+knn_ks = [5, 10, 25, 50, 75, 100]
+embedding_sizes = [5, 10, 50, 100]
+steps_per_epoch = 10
 accuracies_k = []
 
 
 for method in mining_methods:
-    if method == 'batch_all' or method == 'batch_hard':
-        batch_sizes_or_numbers_of_samples = numbers_of_samples_per_class
-    else:
+    if method == 'create_triplet_batch_random':
         batch_sizes_or_numbers_of_samples = batch_sizes
+    else:
+        batch_sizes_or_numbers_of_samples = numbers_of_samples_per_class
+
+    for embedding_size in embedding_sizes:
+        mh = ModelHandler(model_number=5, embedding_size=embedding_size, input_feature_dim=dh.shape)
+        textfile = open('pics/triplet/embedding_size_' + str(embedding_size) + '.txt', 'w')
         
-    for samples_count in batch_sizes_or_numbers_of_samples:
-        for alpha in alphas:
-            for epoch in epochs: 
-                net = TripletNet(mh, dh, alpha)
-                net.print_model()
+        for samples_count in batch_sizes_or_numbers_of_samples:
+            for alpha in alphas:
+                textfile.write('alpha = ' + str(alpha) + ': ')
                 
-                if method == 'batch_all':
-                    K = samples_count # Samples per class.
-                    P = dh.n_classes  # Number of classes.
-                    total_batch_size = P*K*(P*K - K)*(K - 1)
-                    steps_per_epoch = 10 #int(dh.n_train/total_batch_size)
-
-                elif method == 'batch_hard':
-                    K = samples_count # Samples per class.
-                    P = dh.n_classes  # Number of classes.
-                    total_batch_size = P*K
-                    steps_per_epoch = 10#int(dh.n_train/total_batch_size)
-                    #♦epoch = 10
-
-                else:
-                    # A common practice is to set this value to number of samples/batch_size
-                    # so that the model sees the training samples at most once per epoch.
-                    steps_per_epoch = 10#int(dh.n_train/samples_count)
+                for epoch in epochs:
+                    net = TripletNet(mh, dh, alpha)
+                    net.print_model()
+                    
+                    if method == 'batch_all':
+                        K = samples_count # Samples per class.
+                        P = dh.n_classes  # Number of classes.
+                        total_batch_size = P*K*(P*K - K)*(K - 1)
+    
+                    elif method == 'batch_hard':
+                        K = samples_count # Samples per class.
+                        P = dh.n_classes  # Number of classes.
+                        total_batch_size = P*K
+                        
+                    elif method == 'batch_semihard':
+                        K = samples_count # Samples per class.
+                        P = dh.n_classes  # Number of classes.
+                        total_batch_size = P*K*(K - 1)
+    
+                    
+                    # Train model with defined parameters.
+                    history = net.train(method, samples_count, epoch, steps_per_epoch)
+                    #net.save_model()
+                    
+                    # % Plot losses.
+                    plotter.plot_losses(net, history)
                 
-                # Train model with defined parameters.
-                history = net.train(method, samples_count, epoch, steps_per_epoch)
-                #net.save_model()
-                
-                # % Plot loss
-                # Losses
-                plotter.plot_losses(net, history)
-            
-                # % Plot data MNIST
-                # TRAIN DATA ############################################################################
-                plotter.tsne_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
-                # TODO include title when plotting, add stuff like embedding size, model number?, batch size, ....
-            
-                # % TEST DATA #############################################################################
-                plotter.tsne_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='Test data')
-                
-                # % NEW DATA #############################################################################
-                #plotter.pca_plot(X=dh_newdata.X_test, y=dh_newdata.y_test, net=net, suptitle='Different data')
-
-                #% Train kNN classifier with embedded data.
-                X_embedded_train = net.embedding_model.predict(dh.X_train)
-                X_embedded_test  = net.embedding_model.predict(dh.X_test)
-                tmp = []
-                for k in knn_ks:
-                    knn_classifier = KNeighborsClassifier(n_neighbors=k)
-                    knn_classifier.fit(X_embedded_train, dh.y_train)
-                    y_pred = knn_classifier.predict(X_embedded_test)
-                    tmp.append(accuracy_score(dh.y_test, y_pred))
-                accuracies_k.append(tmp)
-                
-                print(alpha)
-                print(accuracies_k)
-# %%
-#dhnew = DataHandler("MNIST", [7,9])
-#plotter.scatter(dhnew.X_train, dhnew.y_train)
-#net.save_model()
-#net1 = TripletNet(model_name='model4_mining-create_triplet_batch_random_alpha0.1_epochs3_batchSize300_steps170')
-
-plotter.pca_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='hgfhfg data')
-#plotter = Plotter()
-#plotter.pca_plot_compare(X=dh.X_train, y=dh.y_train, net=net1, suptitle='Train data')
-#plotter.tsne_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
-#plotter.plot_losses(net, history)
-
-
-   
+                    # % Plot data.
+                    # TRAIN DATA ############################################################################
+                    plotter.tsne_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
+                    plotter.pca_plot_compare(X=dh.X_train, y=dh.y_train, net=net, suptitle='Train data')
+                    
+                    # % TEST DATA #############################################################################
+                    plotter.tsne_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='Test data')
+                    plotter.pca_plot_compare(X=dh.X_test, y=dh.y_test, net=net, suptitle='Test data')
+    
+    
+                    #% Train kNN classifier with embedded data.
+                    X_embedded_train = net.embedding_model.predict(dh.X_train)
+                    X_embedded_test  = net.embedding_model.predict(dh.X_test)
+                    tmp = []
+                    for k in knn_ks:
+                        knn_classifier = KNeighborsClassifier(n_neighbors=k)
+                        knn_classifier.fit(X_embedded_train, dh.y_train)
+                        y_pred = knn_classifier.predict(X_embedded_test)
+                        tmp.append(accuracy_score(dh.y_test, y_pred))
+                    accuracies_k.append(tmp)
+                    
+                    for element in tmp:
+                        textfile.write(str(element) + ", ")
+                    
+                    print(alpha)
+                    print(accuracies_k)
+                    
+                textfile.write('\n')
+        textfile.close()
